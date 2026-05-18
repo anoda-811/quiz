7b7d029682f12e0314dd9ff24885a6768358613a
anoda <atusiufo@yahoo.co.jp>
2026-05-18 19:02:42 +0900

    8

--- app/ai/page.tsx
+++ app/ai/page.tsx
@@ -1,63 +1,57 @@
-export default function AiPage() {
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function AIPage() {
+  const [theme, setTheme] = useState("");
+  const router = useRouter();
+
+  const handleStart = () => {
+    if (!theme) return;
+
+    router.push(
+      `/play?category=AI出題&theme=${encodeURIComponent(
+        theme
+      )}`
+    );
+  };
+
   return (
-    <div>AI Page</div>
-  )
+    <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+
+      <h1 className="text-4xl mb-8">
+        AIクイズ生成
+      </h1>
+
+      <input
+        value={theme}
+        onChange={(e) =>
+          setTheme(e.target.value)
+        }
+        placeholder="例: デスノート"
+        className="
+          bg-black
+          border border-white
+          px-6 py-3
+          rounded-xl
+          text-center
+        "
+      />
+
+      <button
+        onClick={handleStart}
+        className="
+          mt-8
+          px-8 py-3
+          border border-white
+          rounded-xl
+          hover:bg-white
+          hover:text-black
+        "
+      >
+        START
+      </button>
+    </main>
+  );
 }
-
-// "use client";
-
-// import { useState } from "react";
-// import { useRouter } from "next/navigation";
-
-// export default function AIPage() {
-//   const [theme, setTheme] = useState("");
-//   const router = useRouter();
-
-//   const handleStart = () => {
-//     if (!theme) return;
-
-//     router.push(
-//       `/play?category=AI出題&theme=${encodeURIComponent(
-//         theme
-//       )}`
-//     );
-//   };
-
-//   return (
-//     <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
-
-//       <h1 className="text-4xl mb-8">
-//         AIクイズ生成
-//       </h1>
-
-//       <input
-//         value={theme}
-//         onChange={(e) =>
-//           setTheme(e.target.value)
-//         }
-//         placeholder="例: デスノート"
-//         className="
-//           bg-black
-//           border border-white
-//           px-6 py-3
-//           rounded-xl
-//           text-center
-//         "
-//       />
-
-//       <button
-//         onClick={handleStart}
-//         className="
-//           mt-8
-//           px-8 py-3
-//           border border-white
-//           rounded-xl
-//           hover:bg-white
-//           hover:text-black
-//         "
-//       >
-//         START
-//       </button>
-//     </main>
-//   );
-// }
